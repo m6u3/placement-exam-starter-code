a3cafb61d14c4eca3d755fe3d88df3c05c31c2b3
Miguel Rodriguez <mgarl22@gmail.com>
2021-10-21 18:28:54 -0400

    Midway finish GB, need to finish rest

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,60 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0      // Ginger bread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let yourName = "Miguel Rodriguez" // HINT: Replace this with your own name!
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+// We'll use these variables to track the counts of each cookie type
+let gb = 0 ;     // Ginger bread
+let cc = 0 ;     // Chocolate Chip
+let sugar = 0  ; // Sugar Sprinkle
+let total = 0;
+//first lets get the Cells as variables.
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+let gbQtyCell = document.querySelector('#qty-gb');
+let ccQtyCell = document.querySelector('#qty-cc');
+let sugarQtyCell = document.querySelector('#qty-sugar');
+let totalQtyCell = document.querySelector('#qty-total');
+
+
+// Event listener for clicks on the "+" button for Ginger Bread cookies
+document.querySelector('#add-gb').addEventListener('click', function() {
+    // first we need to add 1 everythime the button is clicked;
+    gb = gb + 1;
+    //now lets update the cell
+    
+    gbQtyCell.textContent = gb;
+    
+    console.log('Ginger bread + button was clicked!', gb);
+    
+    //now lets add the totals
+    total = total + 1;
+    
+    // update the cell
+     totalQtyCell.textContent = total;
+    console.log('total cookies are!', total);
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+//Event listener for the minus button for gb.
+document.querySelector('#minus-gb').addEventListener('click', function(){
+    //To remove the cookie without going below zero we need an if Statement
+    if (gb > 0){
+        //this expression will onlu execute if gb is greater than zero!
+        gb = gb - 1;
+
+        console.log('Ginger bread - button was clicked!', gb);
+        //now update the cell
+        gbQtyCell.textContent = gb;
+
+        //Update totals
+        total = total - 1;
+
+        console.log('total cookies are!', total);
+        //update total cell
+        totalQtyCell.textContent = total;
+    }
+})
+
+// Nice now lets just copy and paste lol, and cahnge v names and elements
